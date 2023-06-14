18c210178b28c1c6c7298779a0340af6e2430c26
Zimi1124 <73144348+Zimi1124@users.noreply.github.com>
2023-06-14 02:09:05 +0200

    Fix for sometimes reappearing products after deleting them from the list
    I encountered an issue where products would sporadically reappear after using the mass delete button. However, I was unable to identify the exact cause as it seemed to be random. Furthermore, the problem never occurred after clearing the cache. To replicate the issue, I had to create and delete multiple products.

--- public/js/productList.js
+++ public/js/productList.js
@@ -1,21 +1,22 @@
 /*==================== BUTTONS OPERATION ====================*/
 // Add
 const add_button = document.getElementById('add-product-btn');
-add_button.addEventListener('click', () => {
-    window.location.href = '/add-product/';
+add_button.addEventListener('click', function () {
+    window.location.pathname = '/add-product'
 });
 
 // Delete
 const delete_button = document.getElementById('delete-product-btn');
-delete_button.addEventListener('click', () => {
+delete_button.addEventListener('click', function () {
     if (anyIsChecked()) {
-        removalAnimation();
-        setTimeout(() => {
+        removaAnimation();
+        setTimeout(function () {
             submitRemovalForm();
         }, 1000);
     } else {
         alert('Select at least one product for delete.');
     }
+
 });
 
 /*==================== REMOVAL PRODUCTS ====================*/
@@ -23,22 +24,11 @@ function submitRemovalForm() {
     document.getElementById("delete_form").submit();
 }
 
-function removalAnimation(callback) {
+function removaAnimation() {
     var items = document.querySelectorAll('.list__item');
-    var itemCount = items.length;
-    var itemsProcessed = 0;
-
     items.forEach(item => {
         const checkbox = item.querySelector(".delete-checkbox");
         if (checkbox.checked) {
-            item.addEventListener('transitionend', () => {
-                item.remove(); // Remove the item from the DOM
-                itemsProcessed++;
-                if (itemsProcessed === itemCount) {
-                    callback(); // Call the callback once all items are processed
-                }
-            });
-
             item.style.opacity = 0;
             item.style.transform = 'scale(.9)';
         }
